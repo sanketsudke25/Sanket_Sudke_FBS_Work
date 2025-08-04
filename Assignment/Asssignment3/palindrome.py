7f2f6724463a83abc1b38dba00e60f531cb7a0b1
Sanket Sudke <166213828+sanketsudke25@users.noreply.github.com>
2025-08-04 16:41:11 +0530

    Update palindrome.py

--- Assignment/Asssignment3/palindrome.py
+++ Assignment/Asssignment3/palindrome.py
@@ -1,9 +1,10 @@
 num = int(input("Enter a three-digit number: "))
 
 if 100 <= num <= 999:
-    if str(num) == str(num)[:-1]:
+    if (num) == (num)
         print(f"{num} is a palindrome.")
     else:
         print(f"{num} is not a palindrome.")
 else:
+
     print("Please enter a valid three-digit number.")
